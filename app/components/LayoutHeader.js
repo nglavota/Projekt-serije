708d142a1cf323a6cfb72bedfded1f14f2c8377c
Nikolina Glavota <nglav635@gmail.com>
2025-05-22 21:02:42 +0200

    Mala izmjena dizajna

--- app/components/LayoutHeader.js
+++ app/components/LayoutHeader.js
@@ -1,7 +1,10 @@
 export default function LayoutHeader() {
   return (
     <header className="text-center py-6 rounded-xl mx-2 mb-6">
-      <h1 className="text-5xl font-bold tracking-wide">Popularne TV serije</h1>
+      <h1 className="text-5xl font-bold tracking-wide">Najnovije TV serije</h1>
+      <p className="mt-8 text-lg text-gray-300 max-w-2xl mx-auto">
+        Otkrij svijet najnovijih serija i pronađi svoje favorite - sve na jednom mjestu! 📺🤩
+      </p>
     </header>
   );
 }
